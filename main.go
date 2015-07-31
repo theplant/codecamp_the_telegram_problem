4b15b902ee867462db90a000dc733039e3004096
bodhi <bodhi@5263.org>
2015-07-31 14:24:41 +1000

    Use Juice and Aaron's implementation

--- main.go
+++ main.go
@@ -4,19 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
-var columnWidth int // How long each line should be
+var columnWidth uint64 // How long each line should be
 
-func handle(line string) {
-	// Implement this
-	fmt.Println(line)
+var line string
+
+func handle(source string) {
+	var words = strings.Split(regexp.MustCompile("\n").ReplaceAllString(source, ""), " ")
+
+	for _, word := range words {
+		if length := len(line + word + " "); length > int(columnWidth) {
+			fmt.Println(line)
+			line = word + " "
+		} else if length == int(columnWidth) {
+			fmt.Println(line + word)
+			line = ""
+		} else {
+			line += word + " "
+		}
+	}
+
+	if source == "\n" {
+		fmt.Println(strings.TrimSuffix(line, " "))
+		line = ""
+	}
 }
 
 func main() {
-	width, _ := strconv.ParseInt(os.Args[1], 10, 8)
-	columnWidth = int(width)
+	columnWidth, _ = strconv.ParseUint(os.Args[1], 10, 8)
 
 	input := bufio.NewReader(os.Stdin)
 
